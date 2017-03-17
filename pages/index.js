82494760df2dcb75f992eb0e05ceee6865d6ca5e
Mike Elliott <melliott@overstock.com>
2017-03-16 23:56:33 -0600

    remove links to unused things

--- pages/index.js
+++ pages/index.js
@@ -18,11 +18,8 @@ export default class Index extends React.Component {
         />
         <Header />
         <h1>
-          Hi MIIIKE
+          Hi Sara!
         </h1>
-        <p>Welcome to your new Gatsby site</p>
-        <h2>Below are some pages showing different capabilities built-in to Gatsby</h2>
-        <h3>Supported file types</h3>
         <ul>
           <li>
             <Link to={prefixLink('/markdown/')}>Markdown</Link>
@@ -31,25 +28,10 @@ export default class Index extends React.Component {
             <Link to={prefixLink('/react/')}>JSX (React components)</Link>
           </li>
           <li>
-            <Link to={prefixLink('/json/')}>JSON</Link>
-          </li>
-          <li>
-            <Link to={prefixLink('/yaml/')}>YAML</Link>
-          </li>
-          <li>
-            <Link to={prefixLink('/toml/')}>TOML</Link>
-          </li>
-        </ul>
-        <h3>Supported CSS processors</h3>
-        <ul>
-          <li>
             <Link to={prefixLink('/postcss/')}>PostCSS</Link>
           </li>
           <li>
             <Link to={prefixLink('/css-modules/')}>CSS Modules</Link>
-          </li>
-          <li>
-            <Link to={prefixLink('/sass/')}>Sass</Link>
           </li>
           <li>
             <Link to={prefixLink('/less/')}>Less</Link>
