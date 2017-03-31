9cb9544968d7dcf04289d79745dc6f28b12a4d0e
Tiffany Fritz <fritzty@gmail.com>
2017-03-30 19:08:29 -0600

    styled header and hero

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -7,7 +7,8 @@ import {
   MenuItem
 } from 'react-bootstrap';
 import './Header.css';
-import logo from 'images/lnl.png';
+import FaShoppingCart from 'react-icons/lib/fa/shopping-cart';
+
 
 export default class Header extends React.Component {
   render() {
@@ -15,8 +16,8 @@ export default class Header extends React.Component {
       <Navbar className="header-menu" collapseOnSelect>
          <Navbar.Header>
            <Navbar.Brand>
-             <a href="#">
-               <img src={logo} alt="logo"/>
+             <a className="logo" href="#">
+               Lipstick n' Lace
              </a>
            </Navbar.Brand>
            <Navbar.Toggle />
@@ -24,13 +25,13 @@ export default class Header extends React.Component {
          <Navbar.Collapse>
            <Nav>
              <NavItem eventKey={1} href="#">Products</NavItem>
-             <NavDropdown eventKey={3} title="About" id="basic-nav-dropdown">
-               <MenuItem eventKey={3.1}>About</MenuItem>
-               <MenuItem eventKey={3.2}>How to Use</MenuItem>
-             </NavDropdown>
+             <NavItem eventKey={2} href="#">About</NavItem>
+             <NavItem eventKey={3} href="#">How to Use</NavItem>
            </Nav>
            <Nav pullRight>
-             <NavItem eventKey={1} href="#">Cart</NavItem>
+             <NavItem eventKey={1} href="#">
+               <FaShoppingCart className='cart-button' />
+             </NavItem>
            </Nav>
          </Navbar.Collapse>
       </Navbar>
--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
 import './Hero.css';
+import lipstick from 'images/flowersLips.png';
 
 export default class Hero extends React.Component {
   render() {
     return (
       <Jumbotron className="hero">
-    
+        <img className="banner-img" src={lipstick} alt="lipstick"/>
       </Jumbotron>
     );
   }
